19664b74222953f075b3eb9ff4ba4e7648175dd6
Rachmizard <rachmizard11072000@gmail.com>
2018-11-23 14:49:35 +0700

    CURD MASTER ASRAMA

---
+++ resources/assets/js/components/Sekretariat/Asrama/ListAsramaComponent.vue
@@ -0,0 +1,126 @@
+<template>
+	 <!-- Panel Table Tools -->
+      <div class="panel" id="app">
+    <!-- MODAL -->
+      <div id="deleteModalAsrama" class="modal fade" tabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+              <h4 class="modal-title" id="title-data"></h4>
+            </div>
+            <div class="modal-body" style="margin-bottom: 50px;">
+                <h5>Anda yakin ingin menghapus data tersebut?</h5>
+            </div>
+              <div class="modal-footer">
+                <div class="btn-group">
+                  <button class="btn btn-md btn-info" data-dismiss="modal">Tidak</button>
+                  <button class="btn btn-md btn-danger" id="deleteBtnAsrama"><i class="fa fa-upload"></i><i class="icon wb-trash"></i> Ya</button>
+                </div>
+              </div>
+          </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
+      </div><!-- /.modal -->
+    <!-- END MODAL -->
+        <header class="panel-heading">
+          <h3 class="panel-title">Table Data Asrama</h3>
+        </header>
+        <div class="panel-body">
+          <table class="table table-hover dataTable table-striped w-full" id="asramaTable">
+            <thead>
+              <tr>
+                <th>ID Asrama</th>
+                <th>Nama Asrama</th>
+                <th>Rai'sam Asrama</th>
+                <th>Aksi</th>
+              </tr>
+            </thead>
+            <tfoot>
+              <tr>
+                <th>ID Asrama</th>
+                <th>Nama Asrama</th>
+                <th>Rai'sam Asrama</th>
+                <th>Aksi</th>
+              </tr>
+            </tfoot>
+            <tbody>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <!-- End Panel Table Tools -->
+</template>
+<script>
+
+  import JQuery from 'jquery'
+
+   $(function() {
+         var table = $('#asramaTable').DataTable({
+              processing: true,
+              serverSide: true,
+              ajax: "/sekretariat/asrama/getAsramaDataTables",
+              columns: [
+                  { data: 'id', name: 'id' },
+                  { data: 'nama_asrama.nama_asrama', name: 'nama_asrama.nama_asrama' },
+                  { data: 'roisam_asrama', name: 'roisam_asrama' },
+                  { data: 'action', name: 'action', orderable: false, searchable: false },
+              ]
+          }); 
+        // Trigger auto refresh
+        Echo.channel('draw-table')
+        .listen('DrawTable', (e) => {
+          table.draw();
+        });
+
+        $('#deleteModalAsrama').on('show.bs.modal', function(e) {
+            var id = $(e.relatedTarget).data('id');
+            // $.get('/sekretariat/kelas/'+ id +'/destroy', function( data ) {
+            //   $('#submitDeleteKelas').attr('action', '/sekretariat/kelas/'+ id +'/destroy');
+            // }); 
+          $("#deleteBtnAsrama").on('click', function(){
+              axios.post('/sekretariat/asrama/'+ id +'/destroy').then(function(resp){
+                $('#deleteModalAsrama').modal('hide')
+                table.draw()
+              }).then(function(){
+                window.location.reload()
+              })
+          });
+        });
+  });
+
+   $(document).ready(function(){
+        $('#editModalAsrama').on('show.bs.modal', function(e) {
+                var id = $(e.relatedTarget).data('id');
+                $.get('/sekretariat/asrama/'+ id +'/show', function( data ) {
+                $("#nama_asrama").val(data.nama_asrama);
+                $("#kategori_asrama").val(data.kategori_asrama);
+                $("#roisam_asrama").attr('value', data.roisam_asrama);
+                });
+
+              $('#submitEditAsrama').attr('action', '/sekretariat/asrama/'+ id +'/update');
+        });
+      });
+	export default {
+		data(){
+			return {
+				kelas: []
+			}
+		},
+
+		mounted(){
+			this.listsKelas();	
+			$(document).ready(function(){
+				$('#exampleTableTools').DataTable();
+			});
+		},
+
+		methods: {
+			listsKelas(){
+				let app = this;
+				axios.get('/sekretariat/kelas/JSON').then(response => {
+					app.kelas = response.data;
+				});
+			}
+		}
+	}
+</script>
--- resources/views/layouts/master-layouts.blade.php
+++ resources/views/layouts/master-layouts.blade.php
@@ -113,7 +113,7 @@
                             </a>
                         </li>
                         <li class="nav-item dropdown dropdown-fw dropdown-mega">
-                            <a class="nav-link" data-toggle="dropdown" href="#" aria-expanded="false" data-animation="fade" role="button">Mega <i class="icon wb-chevron-down-mini" aria-hidden="true"></i></a>
+                            <a class="nav-link" data-toggle="dropdown" href="#" aria-expanded="false" data-animation="fade" role="button">Pintas Link  <i class="icon wb-chevron-down-mini" aria-hidden="true"></i></a>
                             <div class="dropdown-menu" role="menu">
                                 <div class="mega-content">
                                     <div class="row">
